6ff1558c7e28365c72a0a29d960d43e1004735a0
mpesut <102237243+mpesut@users.noreply.github.com>
2026-03-17 22:23:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskText = input.value;
+
+    if (taskText === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    const btn = document.createElement("button");
+    btn.textContent = "X";
+    btn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(btn);
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
